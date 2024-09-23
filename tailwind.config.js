40a6a58e205890e12f0dbfd5d30690e44bf5bdd0
davis122991 <110353699+davis122991@users.noreply.github.com>
2024-09-23 19:51:56 +0300

    Deployment build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        border: '#AAAAAA',
+        background: '#292929',
+        text: '#D8D8D8',
+        primary: '#2C1F38',
+        secondary: '#2C1343',
       },
     },
   },
