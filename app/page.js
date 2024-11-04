385146adaff96897d2a2d21935982afc76c3cc9a
davis122991 <110353699+davis122991@users.noreply.github.com>
2024-11-04 19:01:38 +0200

    updated design

--- app/page.js
+++ app/page.js
@@ -1,19 +1,12 @@
 const Page = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-2'>
+    <div className='flex flex-col justify-center items-center gap-2 relative'>
+      <div className='absolute bg-black h-[110%] top-[-5%] z-[-1] w-[105%] blur-lg opacity-50' />
       <h3 className='uppercase text-3xl md:text-5xl text-center font-bold tracking-wider'>
         Tune into the music soon
       </h3>
-      {/* <div className='mt-5 md:mt-10'>
-        <p className='text-center text-opacity-70 text-sm md:text-base'>
-          Currently working on new website.
-        </p>
-        <p className='text-center text-opacity-70 text-sm md:text-base w-full sm:w-[400px]'>
-          Enter your email and click "Notify Me!" to be ready for the Grand
-          Opening!
-        </p>
-      </div> */}
       <form className='flex gap-2 flex-col items-center mt-5 md:mt-10'>
+        {/* <div className='absolute bg-black w-full h-[115%] top-[-15%] z-[-1] md:w-[420px] blur-md opacity-60' /> */}
         <input
           type='email'
           placeholder='Enter your email'
